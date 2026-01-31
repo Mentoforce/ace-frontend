bea468240f85a34e79a166cd4f1a33b16d74a601
Yakshit <yakshit.mentoforce@gmail.com>
2026-01-31 11:29:34 +0530

    changes

--- src/components/home/FeaturedProperties.tsx
+++ src/components/home/FeaturedProperties.tsx
@@ -9,10 +9,12 @@ export default function FeaturedProperties() {
   const t = useTranslations();
 
   return (
-    <section className="px-6 py-10">
-      <h2 className="mb-6 text-2xl font-bold">{t("home.featured")}</h2>
+    <section className="p-10">
+      <h2 className="mb-6 text-2xl font-bold text-center uppercase">
+        {t("home.featured")}
+      </h2>
 
-      <div className="grid gap-6 md:grid-cols-5">
+      <div className="grid gap-6 md:grid-cols-4 mx-10">
         {featuredProperties.map((property) => {
           const content =
             property.translations[locale as string] ??
@@ -21,22 +23,24 @@ export default function FeaturedProperties() {
           return (
             <div
               key={property._id}
-              className="rounded-lg border p-4 flex flex-col justify-around"
+              className="rounded-xl flex flex-col justify-around max-w-75 bg-[#fff8e7]"
             >
               <Image
                 src={property.images[0]}
                 alt={content.title}
                 width={200}
                 height={200}
-                className="mb-4 h-35 w-full rounded"
+                className=" h-50 min-w-full object-cover rounded-xl"
               />
-              <h3 className="text-lg font-semibold">{content.title}</h3>
-              <p className="mt-2 text-sm text-gray-600">
-                {content.description}
-              </p>
-              <div className="mt-4 text-sm text-gray-500">
-                {property.bedrooms} {t("property.bedrooms")} · {property.area}{" "}
-                sqft
+              <div className="mx-2 my-4">
+                <h3 className=" text-lg font-semibold">{content.title}</h3>
+                <p className=" mt-1 text-sm text-gray-600">
+                  {content.description}
+                </p>
+                <div className="mt-4 text-sm text-gray-500">
+                  {property.bedrooms} {t("property.bedrooms")} · {property.area}{" "}
+                  sqft
+                </div>
               </div>
             </div>
           );
